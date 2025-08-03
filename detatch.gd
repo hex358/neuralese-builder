4b1dafeb1d0cd9868561fe1af858807a2a387634
Hex358 // Pantone 358C <165776088+hex358@users.noreply.github.com>
2025-08-03 12:46:14 +0500

    .

--- detatch.gd
+++ detatch.gd
@@ -10,14 +10,15 @@ func _get_nodes(args, kwargs = {}) -> Array[Node]:
 	var i: int = 0
 	for _i in args:
 		i += 1
-		var new: BlockComponent = frozen_duplicate.duplicate()
-		new.placeholder = false
-		new.text = "Spline %s"%i
-		var line = _i if _i is Spline else args[_i]
-		new.metadata["inst"] = line
-		new.metadata["all"] = false
-		lines.append(line)
-		output.append(new)
+		for j in 5:
+			var new: BlockComponent = frozen_duplicate.duplicate()
+			new.placeholder = false
+			new.text = "Spline %s"%i
+			var line = _i if _i is Spline else args[_i]
+			new.metadata["inst"] = line
+			new.metadata["all"] = false
+			lines.append(line)
+			output.append(new)
 	if len(args) >= 3:
 		var new: BlockComponent = frozen_duplicate.duplicate()
 		new.placeholder = false
